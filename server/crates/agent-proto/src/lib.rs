4746176b9e658d339677f115a8e090ba474f6bff
X_Tu <1206294056@qq.com>
2026-02-20 16:19:18 +0800

    T1.1: proto compilation with tonic-build

--- server/crates/agent-proto/src/lib.rs
+++ server/crates/agent-proto/src/lib.rs
@@ -1,1 +1,11 @@
-pub mod proto;
+pub mod ai {
+    pub mod agent {
+        pub mod platform {
+            pub mod v1 {
+                tonic::include_proto!("ai.agent.platform.v1");
+            }
+        }
+    }
+}
+
+pub use ai::agent::platform::v1::*;
