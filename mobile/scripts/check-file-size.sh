10d50fc1f83bff7ed26a1f6603e1f948545dfe87
X_Tu <1206294056@qq.com>
2026-02-22 05:09:52 +0800

    fix(mobile): address MQG2 review issues (block comments, string literals, CI lint)

--- mobile/scripts/check-file-size.sh
+++ mobile/scripts/check-file-size.sh
@@ -11,21 +11,33 @@ violations=0
 while IFS= read -r -d '' file; do
   if ! awk -v file="$file" -v max_file_lines="$max_file_lines" -v max_fn_lines="$max_fn_lines" '
     function is_effective(line) {
-      return line !~ /^[[:space:]]*$/ && line !~ /^[[:space:]]*\/\//
+      return line !~ /^[[:space:]]*$/
     }
 
-    function strip_line_comments(line,   out, i, ch, next_ch, in_str, esc) {
+    function strip_comments(line,   out, i, ch, next_ch, in_str, esc) {
       out = ""
       in_str = 0
       esc = 0
+      i = 1
 
-      for (i = 1; i <= length(line); i++) {
+      while (i <= length(line)) {
         ch = substr(line, i, 1)
         next_ch = (i < length(line) ? substr(line, i + 1, 1) : "")
+
+        if (in_block_comment) {
+          if (ch == "*" && next_ch == "/") {
+            in_block_comment = 0
+            i += 2
+          } else {
+            i++
+          }
+          continue
+        }
 
         if (esc) {
           out = out ch
           esc = 0
+          i++
           continue
         }
 
@@ -33,12 +45,14 @@ while IFS= read -r -d '' file; do
           out = out ch
           if (ch == "\\") esc = 1
           else if (ch == "\"") in_str = 0
+          i++
           continue
         }
 
         if (ch == "\"") {
           out = out ch
           in_str = 1
+          i++
           continue
         }
 
@@ -46,14 +60,20 @@ while IFS= read -r -d '' file; do
           break
         }
 
+        if (ch == "/" && next_ch == "*") {
+          in_block_comment = 1
+          i += 2
+          continue
+        }
+
         out = out ch
+        i++
       }
 
       return out
     }
 
-    function count_braces(line,   cleaned, i, ch, in_str, esc, c) {
-      cleaned = strip_line_comments(line)
+    function count_braces(cleaned,   i, ch, in_str, esc, c) {
       c = 0
       in_str = 0
       esc = 0
@@ -84,14 +104,44 @@ while IFS= read -r -d '' file; do
       return c
     }
 
+    function has_structural_open_brace(cleaned,   i, ch, in_str, esc) {
+      in_str = 0
+      esc = 0
+
+      for (i = 1; i <= length(cleaned); i++) {
+        ch = substr(cleaned, i, 1)
+
+        if (esc) {
+          esc = 0
+          continue
+        }
+
+        if (in_str) {
+          if (ch == "\\") esc = 1
+          else if (ch == "\"") in_str = 0
+          continue
+        }
+
+        if (ch == "\"") {
+          in_str = 1
+          continue
+        }
+
+        if (ch == "{") return 1
+      }
+
+      return 0
+    }
+
     {
       line = $0
-      clean = strip_line_comments(line)
+      clean = strip_comments(line)
 
-      if (is_effective(line)) {
+      if (is_effective(clean)) {
         file_effective++
       }
 
+      # NOTE: This regex does not cover backticked names (`func `default`()`) or operator functions.
       if (!in_fn && !pending_fn && clean ~ /(^|[^[:alnum:]_])func[[:space:]]+[[:alpha:]_][[:alnum:]_]*[[:space:]]*\(/) {
         fn_name = clean
         sub(/.*(^|[^[:alnum:]_])func[[:space:]]+/, "", fn_name)
@@ -101,7 +151,7 @@ while IFS= read -r -d '' file; do
         pending_start = NR
       }
 
-      if (pending_fn && index(clean, "{") > 0) {
+      if (pending_fn && has_structural_open_brace(clean)) {
         in_fn = 1
         fn_name = pending_name
         fn_start = pending_start
@@ -111,11 +161,11 @@ while IFS= read -r -d '' file; do
       }
 
       if (in_fn) {
-        if (is_effective(line)) {
+        if (is_effective(clean)) {
           fn_effective++
         }
 
-        brace_depth += count_braces(line)
+        brace_depth += count_braces(clean)
 
         if (brace_depth == 0) {
           if (fn_effective > max_fn_lines) {
