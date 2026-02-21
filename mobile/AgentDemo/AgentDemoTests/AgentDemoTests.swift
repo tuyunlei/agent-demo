29e506e5ed95833c1af522601f0d2c55d08c5fea
X_Tu <1206294056@qq.com>
2026-02-22 02:56:25 +0800

    fix: correct SwiftLint/SwiftFormat exclude paths and lint issues

--- mobile/AgentDemo/AgentDemoTests/AgentDemoTests.swift
+++ mobile/AgentDemo/AgentDemoTests/AgentDemoTests.swift
@@ -9,9 +9,7 @@ import Testing
 @testable import AgentDemo
 
 struct AgentDemoTests {
-
     @Test func example() async throws {
         // Write your test here and use APIs like `#expect(...)` to check expected conditions.
     }
-
 }
--- mobile/AgentDemo/Sources/GRPCClient/APIClient.swift
+++ mobile/AgentDemo/Sources/GRPCClient/APIClient.swift
@@ -1,4 +1,3 @@
-import Foundation
 import GRPCCore
 import GRPCNIOTransportHTTP2TransportServices
 
